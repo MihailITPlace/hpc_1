487d9eff4cbd4b03c045a3d7cc82a67d11ab5731
msedlyarskiy <ya.halo-halo@yandex.ru>
2021-11-28 21:52:40 +0400

    fixes

--- cmake-build-debug/run.sh
+++ cmake-build-debug/run.sh
@@ -3,7 +3,7 @@
 for ((i = 1; i < 5; i++ )); do
   echo "$i" attempt:
   for ((j=1; j < 17; j++)) do
-    echo run app for "$i" threads...
+    echo run app for "$j" threads...
     ./hpc_1 "$i" > attempt_"$j"_threads_"$j"
   done
 done
